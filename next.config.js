9364db3d671527b89b02521209ef3858122b2ff0
Yurenko <didenkoyura225@gmail.com>
2022-08-17 10:02:15 +0300

    Change config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-poweredByHeader:false,
-optimizeFonts: false,
-env: {
-  APP_URL: process.env.REACT_APP_URL,
-  APP_ENV: process.env.REACT_APP_ENV,
-  APP_SERVER_URL: process.env.REACT_APP_SERVER_URL
-},
+	images: {
+		loader: 'static',
+	},
+	poweredByHeader: false,
+	optimizeFonts: false,
+	env: {
+		APP_URL: process.env.REACT_APP_URL,
+		APP_ENV: process.env.REACT_APP_ENV,
+		APP_SERVER_URL: process.env.REACT_APP_SERVER_URL,
+	},
 
-async rewrites() {
-  return [
-    {
-      source: '/api/:path*',
-      destination: 'https://powerful-stream-29803.herokuapp.com/api/:path*'
-    },
-    {
-      source: '/uploads/:path*',
-      destination: 'https://powerful-stream-29803.herokuapp.com/uploads/:path*'
-    }
-  ]
-}
-
+	async rewrites() {
+		return [
+			{
+				source: '/api/:path*',
+				destination: 'https://powerful-stream-29803.herokuapp.com/api/:path*',
+			},
+			{
+				source: '/uploads/:path*',
+				destination:
+					'https://powerful-stream-29803.herokuapp.com/uploads/:path*',
+			},
+		]
+	},
 }
 
 module.exports = nextConfig
