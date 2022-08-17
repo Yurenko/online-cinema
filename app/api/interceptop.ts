6cdfc1300413ea9b890b757bbc1ce04d9a90b205
Yurenko <didenkoyura225@gmail.com>
2022-08-17 18:55:03 +0300

    Fix bug

--- app/api/interceptop.ts
+++ app/api/interceptop.ts
@@ -16,7 +16,7 @@ export const axiosClassic = axios.create({
 })
 
 export const instance = axios.create({
-	baseURL: API_URL,
+	baseURL: IS_PRODUCTION ? API_SERVER_URL : API_URL,
 	headers: getContentType(),
 })
 instance.interceptors.request.use((config) => {
